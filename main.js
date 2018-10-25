7f40bf400f5c0e687af4468d67a02a0778a0d187
Siddharth <siddharthpadwal3@gmail.com>
2018-10-25 17:55:46 +0530

    saving input field data in firebase collections

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
+var config = {
+    apiKey: "AIzaSyBzcYUHV1o1Eo9EMIqdPleW-IYGi2cQnCU",
+    authDomain: "contactform-f99cc.firebaseapp.com",
+    databaseURL: "https://contactform-f99cc.firebaseio.com",
+    projectId: "contactform-f99cc",
+    storageBucket: "contactform-f99cc.appspot.com",
+    messagingSenderId: "710627465455"
+  };
+  firebase.initializeApp(config);
+
+  //reference messages collection.
+   var messagesRef = firebase.database().ref('messages');
+
 // listen for form submit.
 
 document.getElementById('contactForm').addEventListener('submit',submitForm);
 
-
+//Submit form.
 function submitForm(e){
     e.preventDefault();
 
@@ -13,11 +26,24 @@ function submitForm(e){
     var phone = getInputVal('phone');
     var message = getInputVal('message');
 
-
+    //Save message.
+    saveMessage(name,company,email,phone,message);
 }
 
 // Function to get form values.
 
 function getInputVal(id){
     return document.getElementById(id).value;
+}
+
+// Save messages to firebase.
+function saveMessage(name,company,email,phone,message){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        name: name,
+        company: company,
+        email: email,
+        phone: phone,
+        message: message
+    });
 }
